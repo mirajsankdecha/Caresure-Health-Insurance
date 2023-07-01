bdce6535db8b1bd0ac07fae8a0d5ec003f3a6533
“mirajsankdecha” <mirajsankdecha@gmail.com>
2023-07-01 20:47:56 +0530

    Navbar Created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Pages/Navbar/Navbar.js'
+
+const App = () => {
+  return (
+    <>  
+      <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Navbar/Navbar.js
@@ -0,0 +1,109 @@
+import React from "react";
+import { RiCustomerService2Line } from "react-icons/ri";
+import logo from "../../image/logo.png";
+const Navbar = () => {
+  return (
+    <div>
+      <>
+        <nav className="relative px-4 py-4 flex justify-between items-center bg-white">
+          <a href="/" className="flex items-center">
+            <img src={logo} className="h-10 mr-3" alt="Logo" />
+          </a>
+          <div className="lg:hidden">
+            <button className="navbar-burger flex items-center text-blue-600 p-3">
+              <svg
+                className="block h-4 w-4 fill-current"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Mobile menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              </svg>
+            </button>
+          </div>
+          <ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
+            <li>
+              <a className="text-sm text-gray-400 hover:text-gray-500" href="/">
+                Plans
+              </a>
+            </li>
+            <li className="text-gray-300">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                stroke="currentColor"
+                className="w-4 h-4 current-fill"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+                />
+              </svg>
+            </li>
+            <li>
+              <a className="text-sm text-blue-600 font-bold" href="/">
+                Renew
+              </a>
+            </li>
+            <li className="text-gray-300">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                stroke="currentColor"
+                className="w-4 h-4 current-fill"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+                />
+              </svg>
+            </li>
+            <li>
+              <a className="text-sm text-gray-400 hover:text-gray-500" href="/">
+                Claims
+              </a>
+            </li>
+            <li className="text-gray-300">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                stroke="currentColor"
+                className="w-4 h-4 current-fill"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+                />
+              </svg>
+            </li>
+            <li>
+              <a className="text-sm text-gray-400 hover:text-gray-500" href="/">
+                About Us
+              </a>
+            </li>
+            <li className="text-gray-300"></li>
+          </ul>
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600  flex items-center"
+            href="tel:+91 8511411930"
+          >
+            <RiCustomerService2Line className="mr-1" />
+            <span>Call Us</span>
+          </button>
+        </nav>
+      </>
+    </div>
+  );
+};
+
+export default Navbar;
