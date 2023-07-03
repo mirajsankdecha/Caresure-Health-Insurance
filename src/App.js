153925cd7034402ef5955239107e46897905c2bd
“mirajsankdecha” <mirajsankdecha@gmail.com>
2023-07-03 21:29:42 +0530

    Footer Created and Routes Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react'
-import Navbar from './Pages/Navbar/Navbar.js'
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Pages/Navbar/Navbar.js";
+import Home from "./Pages/Home/Home.js";
+import Plans from "./Pages/Plans/Plans.js";
+import Renew from "./Pages/Renew/Renew.js";
+import Claims from "./Pages/Claims/Claims.js";
+import About from "./Pages/About/About.js";
+import Footer from "./Pages/Footer/Footer.js";
 const App = () => {
   return (
-    <>  
-      <Navbar />
+    <>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <Routes>
+          <Route path="/" index element={<Home />} />
+          <Route path="/plans" element={<Plans />} />
+          <Route path="/renew" element={<Renew />} />
+          <Route path="/claims" element={<Claims />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/Pages/Renew/Renew.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Renew = () => {
+  return (
+    <div>Renew</div>
+  )
+}
+
+export default Renew
